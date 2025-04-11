c9573312d932aff9bc28e812410c27f8e670dbe0
silaswpo <silaswesleypereiradeoliveira@gmail.com>
2025-04-11 16:08:24 -0300

    Codigo co duas possibilidades de solução

--- binary_to_gray.v
+++ binary_to_gray.v
@@ -1,8 +1,19 @@
 module Binary_to_Gray (
     input  wire [3:0] binary_in,
-    output wire [3:0] gray_out
+    output  [3:0] gray_out
 );
 
-// Insira seu codigo aqui
+/*always @(*) begin
+
+    gray_out[3]=binary_in[3];
+    gray_out[2]=binary_in[3]^binary_in[2];
+    gray_out[1]=binary_in[2]^binary_in[1];
+    gray_out[0]=binary_in[1]^binary_in[0];
+
+end */
+
+
+
+ assign gray_out= {binary_in[3],binary_in[3]^binary_in[2],binary_in[2]^binary_in[1],binary_in[1]^binary_in[0]};
 
 endmodule
--- gray_to_binary.v
+++ gray_to_binary.v
@@ -1,8 +1,19 @@
 module Gray_to_Binary (
     input  wire [3:0] gray_in,
-    output wire [3:0] binary_out
+    output [3:0] binary_out
 );
 
-// Insira seu codigo aqui
+/*always @(*) begin
+
+    binary_out[3]=gray_in[3];
+    binary_out[2]=binary_out[3]^gray_in[2];
+    binary_out[1]=binary_out[2]^gray_in[1];
+    binary_out[0]=binary_out[1]^gray_in[0];
+
+end */   
+
+
+
+ assign binary_out= {gray_in[3],binary_out[3]^gray_in[2],binary_out[2]^gray_in[1],binary_out[1]^gray_in[0]};
 
 endmodule
